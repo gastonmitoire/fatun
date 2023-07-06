1b29af0df9bf48ef51de02f1b4fd4e1d9f805782
Gastón Mitoire <gastonmm@gmail.com>
2023-07-06 00:50:19 -0300

    feat: images array

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,29 +3,29 @@ import React from "react";
 import Carousel from "../components/Carousel";
 
 export default function index() {
+  const images = [
+    {
+      url: "https://picsum.photos/800/400?random=1",
+      caption: "Caption 1",
+      link: "https://google.com",
+    },
+    {
+      url: "https://picsum.photos/800/400?random=2",
+      caption: "Caption 2",
+      link: "https://google.com",
+    },
+    {
+      url: "https://picsum.photos/800/400?random=3",
+      caption: "Caption 3",
+      link: "https://google.com",
+    },
+  ];
+
   return (
     <div className="container mx-auto">
       <h1>Index</h1>
 
-      <Carousel
-        images={[
-          {
-            url: "https://picsum.photos/800/400?random=1",
-            caption: "Caption 1",
-            link: "https://google.com",
-          },
-          {
-            url: "https://picsum.photos/800/400?random=2",
-            caption: "Caption 2",
-            link: "https://google.com",
-          },
-          {
-            url: "https://picsum.photos/800/400?random=3",
-            caption: "Caption 3",
-            link: "https://google.com",
-          },
-        ]}
-      />
+      <Carousel images={images} />
     </div>
   );
 }
