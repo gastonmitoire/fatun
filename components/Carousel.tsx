cb81dbe610ac7afa3721a72f1064e8ea7ada8b97
Gastón Mitoire <gastonmm@gmail.com>
2023-07-06 00:49:13 -0300

    feat: carousel init

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 interface CarouselImage {
   url: string;
   caption: string;
-  link?: string;
+  link: string;
 }
 
 interface CarouselProps {
@@ -28,12 +28,26 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
     }
   };
 
-  const handlePrev = () => {
+  const handlePrev = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
     if (currentImage === 0) {
       setCurrentImage(images.length - 1);
     } else {
       setCurrentImage(currentImage - 1);
     }
+  };
+
+  const handleImageClick = (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    window.open(images[currentImage].link, "_blank");
   };
 
   return (
@@ -52,28 +66,79 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
         <img
           src={images[currentImage].url}
           alt={images[currentImage].caption}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover object-center cursor-pointer"
+          onClick={handleImageClick}
         />
-        <div
+
+        <button
           className="
             absolute
-            top-0
-            left-0
-            w-full
-            h-full
-            flex
-            justify-between
-            items-center
-            px-4
+            top-1/2
+            left-3
+            transform
+            -translate-y-1/2
+            bg-black
+            bg-opacity-50
             text-white
-            text-2xl
-            font-bold
-            cursor-pointer
-        "
+            px-2
+            py-2
+            rounded-full
+            hover:bg-opacity-75
+            focus:outline-none
+            "
+          onClick={handlePrev}
         >
-          <button onClick={handlePrev}>Prev</button>
-          <button onClick={handleNext}>Next</button>
-        </div>
+          <svg
+            aria-hidden="true"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+          >
+            <path
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </button>
+
+        <button
+          className="
+            absolute
+            top-1/2
+            right-3
+            transform
+            -translate-y-1/2
+            bg-black
+            bg-opacity-50
+            text-white
+            px-2
+            py-2
+            rounded-full
+            hover:bg-opacity-75
+            focus:outline-none
+            "
+          onClick={handleNext}
+        >
+          <svg
+            aria-hidden="true"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+          >
+            <path
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,17 @@ export default function index() {
           {
             url: "https://picsum.photos/800/400?random=1",
             caption: "Caption 1",
+            link: "https://google.com",
           },
           {
             url: "https://picsum.photos/800/400?random=2",
             caption: "Caption 2",
+            link: "https://google.com",
           },
           {
             url: "https://picsum.photos/800/400?random=3",
             caption: "Caption 3",
+            link: "https://google.com",
           },
         ]}
       />
